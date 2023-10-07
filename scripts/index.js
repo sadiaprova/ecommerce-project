35e3edda05c7ba7873d9bae363b7d92b9ccfa174
sadiaprova <sadiaprova621@gmail.com>
2023-10-07 18:57:27 +0600

    bag store

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023 . All rights reserved.
+ */
+
+const menuIcon = document.getElementById
+("menu-icon");
+const menu= document.getElementById("menu");
+
+menuIcon?.addEventListener("click",()=>{
+    if(menu.className="hidden"){
+        menu.classList.remove("hidden");
+    }
+        else{
+        menu.classList.add("hidden");
+    }
+
+    
+    
+    
+    });
+
